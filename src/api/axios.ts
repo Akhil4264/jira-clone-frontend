525bb72b5fdcd9db303ef058c5ed535bbed0f0c2
Akhil4264 <93485154+Akhil4264@users.noreply.github.com>
2024-09-14 11:52:15 +0530

    removed localhost connections

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosDf = axios.create({
 	// baseURL: 'https://jira-clone.onrender.com/',
-	baseURL: 'http://localhost:4000/',
+	baseURL: "http://ec2-3-110-84-3.ap-south-1.compute.amazonaws.com",
 	withCredentials: true,
 })
 
