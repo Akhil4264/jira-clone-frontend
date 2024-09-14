c76da75c37a9d54fe8bbc0c16d26b2aa5daba25b
Akhil4264 <93485154+Akhil4264@users.noreply.github.com>
2024-09-14 12:44:58 +0530

    added nginx.conf file

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
 	reducerPath: 'jiraApiReducer',
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://ec2-3-110-84-3.ap-south-1.compute.amazonaws.com",
+		baseUrl: "http://ec2-3-110-84-3.ap-south-1.compute.amazonaws.com/",
 		credentials: 'include',
 	}),
 	tagTypes: ['Lists', 'Issues', 'Project', 'Members', 'AuthUser', 'Comments'],
