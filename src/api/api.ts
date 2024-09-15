25d212960675a1a6140512c1d105141ebde3c3e5
Akhil4264 <93485154+Akhil4264@users.noreply.github.com>
2024-09-15 09:43:13 +0530

    changed api endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
 	reducerPath: 'jiraApiReducer',
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://ec2-43-204-218-102.ap-south-1.compute.amazonaws.com/api",
+		baseUrl: "http://ec2-43-204-218-102.ap-south-1.compute.amazonaws.com:8080/api",
 		// baseUrl: "http://localhost:4000/api",
 		credentials: 'include',
 	}),
